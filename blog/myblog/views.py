eca55a1e0abab1e096ce263ce3d4430fd431faaa
ぐぇん <caonguyen26032001@gmail.com>
2024-11-11 16:37:14 +0900

    Nguyên - giải thích code ở file views.py

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -12,63 +12,100 @@ from .models import Category, Post, Comment
 from .forms import ContactForm, PostForm, CommentForm
 
 class AllCategoryMixin:
-    '''View này để Mixin nhằm lấy danh sách danh mục (category list)'''
     def get_context_data(self, **kwargs):
+        # Gọi phương thức get_context_data của lớp cha để lấy context ban đầu
         context = super().get_context_data(**kwargs)
-        # lấy toàn bộ danh mục gán vào context["categories"]
+
+        # Thêm tất cả các đối tượng Category vào context với khóa 'categories'
         context["categories"] = Category.objects.all()
 
+        # Trả về context đã được bổ sung
         return context
 
 class IndexView(AllCategoryMixin, ListView):
-    '''View này là trang chính'''
+    # Template sẽ được render cho trang chính
     template_name = 'myblog/index.html'
-    
+
+    # Số lượng bài đăng hiển thị trên mỗi trang (phân trang)
     paginate_by = 3
 
     def get_queryset(self):
+        # Truy vấn các bài đăng từ cơ sở dữ liệu, sắp xếp theo ngày đăng (mới nhất lên đầu)
         queryset = Post.objects.order_by('-posted_at')
+        
+        # Trả về queryset các bài đăng đã được sắp xếp
         return queryset
 
     def get_context_data(self, **kwargs):
+        # Lấy các context cơ bản từ phương thức cha (ListView)
         context = super().get_context_data(**kwargs)
 
+        # Thêm thông tin người dùng hiện tại vào context
         context['current_user'] = self.request.user
 
         return context
     
 class CategorySearchView(AllCategoryMixin, ListView):
-    ''' View này để tìm kiếm theo danh mục (search by category)'''
+    # Template sẽ hiển thị kết quả tìm kiếm theo danh mục
     template_name = "myblog/search-result.html"
 
     def get_queryset(self):
+        # Lấy ID danh mục từ URL kwargs (chắc chắn rằng URL được cấu hình đúng)
         c_id = self.kwargs["category"]
-        # lấy data mà id category trong Post（ForeignKey:Category）có cùng với c_id
+
+        # Lọc các bài đăng (Post) có category_id trùng với ID danh mục
         products = Post.objects.filter(category_id=c_id)
+
+        # Trả về danh sách bài đăng thuộc danh mục đó
         return products
 
 class PostDetailView(DetailView):
+    # Template để render chi tiết bài đăng
     template_name = "myblog/post-detail.html"
     
+    # Model mà view này sẽ làm việc, ở đây là Post
     model = Post
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['current_user'] = self.request.user
-        context['comments'] = self.object.comments.all()
-        context['form'] = CommentForm()
-        return context
-    
+    # Phương thức POST để xử lý khi người dùng gửi bình luận
     def post(self, request, *args, **kwargs):
+        # Lấy bài đăng hiện tại từ URL (pk)
         post = self.get_object()
+
+        # Khởi tạo form từ dữ liệu POST
         form = CommentForm(request.POST)
+
+        # Kiểm tra nếu form hợp lệ
         if form.is_valid():
+            # Tạo đối tượng bình luận nhưng chưa lưu vào DB
             comment = form.save(commit=False)
+
+            # Gán bài đăng và người dùng vào bình luận
             comment.post = post
             comment.user = request.user
+
+            # Lưu bình luận vào cơ sở dữ liệu
             comment.save()
+
+            # Sau khi lưu bình luận thành công, chuyển hướng lại về trang chi tiết bài đăng
             return redirect('myblog:post-detail', pk=post.pk) 
+
+        # Nếu form không hợp lệ, render lại trang chi tiết bài đăng cùng với form bình luận
         return render(request, self.template_name, {'form': form, 'post': post})
+
+    # Phương thức để thêm các context vào template, bao gồm các thông tin về bài đăng và bình luận
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        context['current_user'] = self.request.user
+
+        # Thêm tất cả các bình luận của bài đăng vào context
+        context['comments'] = self.object.comments.all()
+
+        # Khởi tạo form bình luận trống để hiển thị trên trang
+        context['form'] = CommentForm()
+
+        return context
 
 class AboutView(TemplateView):
     template_name = 'myblog/about.html'
@@ -81,12 +118,54 @@ class AboutView(TemplateView):
         return context
 
 class ContactView(FormView):
-    template_name = 'myblog/contact.html'  
+    # Template HTML sẽ render form liên hệ cho người dùng nhập liệu
+    template_name = 'myblog/contact.html'
 
+    # Form sẽ sử dụng trong view này, là ContactForm
     form_class = ContactForm
 
-    success_url = reverse_lazy("myblog:contact") 
+    # Đường dẫn chuyển hướng sau khi gửi form thành công
+    success_url = reverse_lazy("myblog:contact")
 
+    def form_valid(self, form):
+        # Lấy dữ liệu từ các trường của form sau khi được validate (hợp lệ)
+        name = form.cleaned_data['name']  # Tên người gửi
+        email = form.cleaned_data['email']  # Email của người gửi
+        phonenumber = form.cleaned_data['phonenumber']  # Số điện thoại
+        message = form.cleaned_data['message']  # Nội dung lời nhắn
+
+        # Cách ghi 1 (sử dụng .format())
+        # subject = 'Liên hệ: {}'.format(phonenumber)
+
+        # Cách ghi 2 (sử dụng f-string')
+        subject = f'Liên hệ: {phonenumber}'  # Tiêu đề email
+
+        # Nội dung của email, sử dụng f-string để dễ đọc hơn
+        message = f'Tên người gửi: {name}\nEmail: {email}\nSố điện thoại:{phonenumber}\nLời nhắn:\n{message}' 
+
+        # Địa chỉ email của người gửi
+        from_email = 'caonguyen26032001@gmail.com'
+
+        # Danh sách email người nhận (có thể gửi tới nhiều email)
+        to_list = ['caonguyen26032001@gmail.com']
+
+        # Tạo một EmailMessage với tiêu đề, nội dung, người gửi, và người nhận
+        message = EmailMessage(
+            subject=subject,  # Tiêu đề của email
+            body=message,  # Nội dung của email
+            from_email=from_email,  # Địa chỉ email của người gửi
+            to=to_list,  # Danh sách địa chỉ email cần gửi tới
+        )
+        
+        # Gửi email
+        message.send()
+
+        # Hiển thị thông báo thành công trên giao diện
+        messages.success(self.request, 'Đã gửi thành công!')
+
+        # Chuyển hướng người dùng đến success_url sau khi gửi thành công
+        return super().form_valid(form)
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
@@ -95,64 +174,27 @@ class ContactView(FormView):
 
         return context
 
-
-    def form_valid(self, form):
-        # Đặt biến tạm, lấy data từ forms.py gán vào biến tạm
-        name = form.cleaned_data['name']
-        email = form.cleaned_data['email']
-        phonenumber = form.cleaned_data['phonenumber']
-        message = form.cleaned_data['message']   
-
-        # thiết lập định dạng phonenumber
-         # cách ghi 1
-        # subject = 'Liên hệ: {}'.format(phonenumber)
-
-         # cách ghi 2
-        subject = f'Liên hệ: {phonenumber}'
-
-        # thiết lập định dạng dữ liệu nhập vào form
-         # cách ghi 1
-        # message = 'Tên người gửi: {0}\nEmail: {1}\n Số điện thoại:{2}\n Lời nhắn:\n{3}'.format(name, email, phonenumber, message)
-        
-         # cách ghi 2
-        message = f'Tên người gửi: {name}\nEmail: {email}\nSố điện thoại:{phonenumber}\nLời nhắn:\n{message}' 
-        
-        # Email của người gửi
-        from_email = 'caonguyen26032001@gmail.com'
-        # Danh sách địa chỉ email cần gửi
-        to_list = ['caonguyen26032001@gmail.com']
-
-        # Tạo object EmailMessage
-        message = EmailMessage(subject=subject,#Tiêu đề
-                               body=message, #Nội dung
-                               from_email=from_email, #mail người gửi
-                               to=to_list, #Danh sách địa chỉ email cần gửi tới
-                               )
-        
-        # dùng method .send() của class EmailMessage để gửi mail
-        message.send()
-
-        # message sẽ được hiển thị sau khi gửi thành công 
-        messages.success(self.request, 'Đã gửi thành công!')
-
-        return super().form_valid(form)
-
 class CreatePostView(CreateView):
+    # Khai báo form sẽ sử dụng trong view này, ở đây là PostForm
     form_class = PostForm
+
+    # Template sẽ render form cho người dùng nhập liệu
     template_name = 'myblog/create-post.html'
+
+    # Đường dẫn sẽ chuyển hướng đến sau khi bài đăng được tạo thành công
     success_url = reverse_lazy('myblog:myblog/create-post-done')
 
     def form_valid(self, form):
-        
+        # Tạo một instance của model Post từ form mà không lưu vào DB ngay lập tức
         postdata = form.save(commit=False)
 
+        # Gán người dùng hiện tại làm tác giả của bài đăng
         postdata.user = self.request.user
 
-        print(postdata.category)
-
-
+        # Lưu instance vào DB sau khi đã gán thuộc tính user
         postdata.save()
 
+        # Gọi phương thức form_valid của CreateView để xử lý chuyển hướng
         return super().form_valid(form)
     
     def get_context_data(self, **kwargs):
